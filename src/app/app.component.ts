bd818b5a025445e3aa2a8bba014d23e279de0448
Fabien Dehopré <fabien@dehopre.com>
2022-09-18 13:51:51 +0200

    detect metamask using utility function and use a custom rxjs operator to enter ngzone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { detectEthereumProvider, MetaMaskEthereumProvider } from './detect-provider';
+import { enterZone } from './enter-zone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe, NgIf],
   template: `
-    <p>{{ title }}</p>
+    <ng-container *ngIf="provider != undefined">
+      <button (click)="connectToWallet()">Connect</button>
+      <hr>
+      <pre>Accounts: {{ accounts$ | async | json }}</pre>
+    </ng-container>
   `,
 })
-export class AppComponent {
-  title = 'metamask';
+export class AppComponent implements OnInit {
+  provider?: MetaMaskEthereumProvider | null;
+  accounts$?: Observable<string[]>;
+  accountsSub = new ReplaySubject<string[]>(1);
+
+  constructor(private readonly zone: NgZone) {}
+
+  async ngOnInit(): Promise<void> {
+    this.accounts$ = this.accountsSub.asObservable().pipe(enterZone(this.zone));
+    this.provider = await detectEthereumProvider();
+    if (this.provider) {
+      this.provider.on('accountsChanged', (accounts) => {
+        this.accountsSub.next(accounts);
+      });
+    }
+  }
+
+  async connectToWallet(): Promise<void> {
+    if (this.provider) {
+      const result = (await this.provider.request({ method: 'eth_requestAccounts' })) as string[];
+      console.log('ETH accounts', result);
+      this.accountsSub.next(result);
+    }
+  }
 }
---
+++ src/app/enter-zone.ts
@@ -0,0 +1,28 @@
+import { NgZone } from '@angular/core';
+import { MonoTypeOperatorFunction, Observable } from 'rxjs';
+
+/**
+ * Lets an observable running outside Angular zone to enter into Angular zone so that the change detection works properly.
+ *
+ * @param zone The Angular zone in which the application is running.
+ * @return A new observable running inside the Angular zone.
+ */
+export function enterZone<T>(zone: NgZone): MonoTypeOperatorFunction<T> {
+return (source: Observable<T>) => {
+    return new Observable<T>((observer) => {
+    return source.subscribe({
+        next: (value) => {
+        zone.run(() => {
+            observer.next(value);
+        });
+        },
+        error: (err) => {
+        observer.error(err);
+        },
+        complete: () => {
+        observer.complete();
+        },
+    });
+    });
+};
+}
